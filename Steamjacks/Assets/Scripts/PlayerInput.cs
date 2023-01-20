ee286a5548a1e8914004ecc319dc010ecc0a5d0a
Michael <mkirkpatrick802@gmail.com>
2023-01-20 07:53:57 -0500

    Grappling Gun Started

--- Steamjacks/Assets/Scripts/PlayerInput.cs
+++ Steamjacks/Assets/Scripts/PlayerInput.cs
@@ -5,11 +5,22 @@ using Unity.Netcode;
 
 public class PlayerInput : NetworkBehaviour
 {
+    [Header("References:")]
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private GrapplingGun _grapplingGun;
     private float _movement;
 
     private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+                //Fire
+        else if(Input.GetKey(KeyCode.Mouse0))
+                //Hold
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+                //Release
+        else
+                //Rotate Gun
+
         _movement = Input.GetAxisRaw("Horizontal");
     }
 
